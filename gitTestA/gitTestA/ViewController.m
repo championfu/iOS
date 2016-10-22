dc7e0fe0d3c393a0e14246d9fd18973d113dee38
Sam <Sam@championfu-2.local>
2016-10-22 09:59:46 +0800

    try to push

--- gitTestA/gitTestA/ViewController.m
+++ gitTestA/gitTestA/ViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor magentaColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 

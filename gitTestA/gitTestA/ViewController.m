a926c23b254caaaaf6d448d22b1bcbaab3e78b83
Sam <Sam@championfu-2.local>
2016-10-22 09:49:36 +0800

    changed the view's backgroundColor

--- gitTestA/gitTestA/ViewController.m
+++ gitTestA/gitTestA/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor magentaColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
